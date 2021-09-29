d2c4c97684fd2a9afd9c3423be7bbf60fb249383
hoshito-kishi <91161186+hoshito-kishi@users.noreply.github.com>
2021-09-29 20:26:05 +0900

    css-in-jsの書き方

--- src/components/inputTodo.jsx
+++ src/components/inputTodo.jsx
@@ -2,8 +2,16 @@ import React from "react";
 
 export const InputTodo = (props) => {
   const { todoText, onChange, onClick } = props;
+  const style = {
+    backgroundColor: "#c1ffff",
+    widows: "400px",
+    height: "30px",
+    padding: "8px",
+    margin: "8px",
+    borderRadius: "8px"
+  };
   return (
-    <div className="input-area">
+    <div style={style}>
       <input
         placeholder="TODOを入力して下さい"
         value={todoText}

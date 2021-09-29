ce6417a274df6d7b088cb087c6b42745a2938119
hoshito-kishi <91161186+hoshito-kishi@users.noreply.github.com>
2021-09-29 20:34:02 +0900

    TODOの上限設定実装

--- src/components/inputTodo.jsx
+++ src/components/inputTodo.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export const InputTodo = (props) => {
-  const { todoText, onChange, onClick } = props;
+  const { todoText, onChange, onClick, disabled } = props;
   const style = {
     backgroundColor: "#c1ffff",
     widows: "400px",
@@ -16,6 +16,7 @@ export const InputTodo = (props) => {
         placeholder="TODOを入力して下さい"
         value={todoText}
         onChange={onChange}
+        disabled={disabled}
       />
       <button onClick={onClick}>追加</button>
     </div>
